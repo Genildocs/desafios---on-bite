aed87e425d20edb88240a1e786f0391dba142a23
Genildo <genildocs@gmail.com>
2023-06-02 02:40:24 -0300

    comit

--- easy/desafio7.js
+++ easy/desafio7.js
@@ -11,7 +11,7 @@ function organizaArray(...arrays){
     return novoArray.sort((a,b)=> a - b);
 }
 
-console.log(organizaArray(...[[1,3],[4,8],[7,5],[2,6]]))
+console.log(organizaArray(...[[1,5,3],[4,8],[7,5],[2,6]]))
 console.log(organizaArray(...[[1,5,3],[6,19,11],[47,128,5],[1,93,51,42,103]]))
 console.log(organizaArray(...[[],[],[]]))
 console.log(organizaArray(...[[100,50],[60,100],[20,100,70],[10,40,80,90]]))
